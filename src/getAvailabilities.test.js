9d2d85ea0b9a0016c393f025e044fc63135a9b6a
adimusset <adrien@veyo-care.com>
2018-05-20 18:46:39 +0200

    add some tests for invalid data

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -55,6 +55,14 @@ describe("getAvailabilities", () => {
 
 describe("utils", () => {
     it("should fetch data from database correctly", async () => {
+        await knex("events").insert([
+            {
+                kind: "opening",
+                starts_at: new Date("2014-08-13 9:30"),
+                ends_at: new Date("2014-08-13 20:00"),
+                weekly_recurring: false
+            }
+        ]);
         const allEvents = await fetchEvents(new Date("2014-08-10"));
         expect(allEvents).toEqual({
             appointments: [
@@ -72,10 +80,17 @@ describe("utils", () => {
                     weekly_recurring: 1
                 }
             ],
-            nonRecurringOpenings: []
+            nonRecurringOpenings: [
+                {
+                    kind: "opening",
+                    starts_at: Date.parse("2014-08-13 9:30"),
+                    ends_at: Date.parse("2014-08-13 20:00"),
+                    weekly_recurring: 0
+                }
+            ]
         });
     });
-    it("should convert events to availabilities correctly", async () => {
+    it("should convert events to availabilities correctly", () => {
         const events = {
             appointments: [
                 {
@@ -97,8 +112,8 @@ describe("utils", () => {
             recurringOpenings: [
                 {
                     kind: "opening",
-                    starts_at: Date.parse("2018-05-15 21:00"),
-                    ends_at: Date.parse("2018-05-15 21:30"),
+                    starts_at: Date.parse("2018-05-30 21:00"),
+                    ends_at: Date.parse("2018-05-30 21:30"),
                     weekly_recurring: 1
                 }
             ],
@@ -169,5 +184,92 @@ describe("utils", () => {
                 slots: []
             }
         ]);
+    });
+});
+
+describe("invalid data", () => {
+    it("should ignore invalid appointement", () => {
+        const events = {
+            appointments: [
+                {
+                    kind: "appointment",
+                    starts_at: new Date("2018-05-22 17:30"),
+                    ends_at: new Date("2018-05-22 18:30")
+                }
+            ],
+            recurringOpenings: [],
+            nonRecurringOpenings: [
+                {
+                    kind: "opening",
+                    starts_at: new Date("2018-05-21 9:30"),
+                    ends_at: new Date("2018-05-21 20:00"),
+                    weekly_recurring: false
+                }
+            ]
+        };
+        const availabilities = availabilitiesFromEvents(
+            events,
+            new Date("2018-05-21")
+        );
+        expect(availabilities[0].slots.length).toEqual(21);
+        expect(availabilities[1].slots.length).toEqual(0);
+    });
+    it("should ignore overlapping openings", () => {
+        const events = {
+            appointments: [],
+            recurringOpenings: [
+                {
+                    kind: "opening",
+                    starts_at: new Date("2018-05-21 9:00"),
+                    ends_at: new Date("2018-05-21 11:00"),
+                    weekly_recurring: true
+                }
+            ],
+            nonRecurringOpenings: [
+                {
+                    kind: "opening",
+                    starts_at: new Date("2018-05-21 9:30"),
+                    ends_at: new Date("2018-05-21 12:00"),
+                    weekly_recurring: false
+                }
+            ]
+        };
+        const availabilities = availabilitiesFromEvents(
+            events,
+            new Date("2018-05-21")
+        );
+        expect(availabilities[0].slots.length).toEqual(6);
+        expect(availabilities[1].slots.length).toEqual(4);
+    });
+    it("should ignore overlapping appointements", () => {
+        const events = {
+            appointments: [
+                {
+                    kind: "appointment",
+                    starts_at: new Date("2018-05-21 9:00"),
+                    ends_at: new Date("2018-05-21 11:00")
+                },
+                {
+                    kind: "appointment",
+                    starts_at: new Date("2018-05-21 10:00"),
+                    ends_at: new Date("2018-05-21 11:30")
+                }
+            ],
+            recurringOpenings: [],
+            nonRecurringOpenings: [
+                {
+                    kind: "opening",
+                    starts_at: new Date("2018-05-21 9:00"),
+                    ends_at: new Date("2018-05-21 12:00"),
+                    weekly_recurring: false
+                }
+            ]
+        };
+        const availabilities = availabilitiesFromEvents(
+            events,
+            new Date("2018-05-21")
+        );
+        expect(availabilities[0].slots.length).toEqual(1);
+        expect(availabilities[1].slots.length).toEqual(0);
     });
 });
